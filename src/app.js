270dc5ef9f1f393a20341c9f4e1092e1878ac46c
Pedro <pedropiacentini@usp.br>
2023-04-28 18:44:05 -0300

    feat:cadastro/login

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import router from './routes/index.routes.js'
+
 
 
 const app = express();
 const PORT = 5000;
 
-app.use(json());
+app.use(express.json());
 app.use(cors());
+app.use(Router)
 
 // eslint-disable-next-line no-undef
 
 
 //Blocos de código do projeto
 
-app.listen(PORT, () => print(`Server online in port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server online in port: ${PORT}`));
---
+++ src/routes/user.rotes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import { userSchema, loginSchema } from "../schemas/user.schema.js";
+import { login, signUp } from "../controllers/users.controllers.js";
+
+const authRouter = Router();
+
+authRouter.post("/sign-up", validateSchema(userSchema), signUp);
+authRouter.post("/login", validateSchema(loginSchema), login);
+
+export default authRouter;
