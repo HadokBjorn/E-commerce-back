87367d6db26d5ccaa34673120f951e23716656c4
Pedro <pedropiacentini@usp.br>
2023-04-30 15:08:25 -0300

    fix: login/cadastro

--- src/app.js
+++ src/app.js
@@ -7,13 +7,8 @@ import router from './routes/index.routes.js'
 const app = express();
 const PORT = 5000;
 
-app.use(express.json());
 app.use(cors());
-app.use(Router)
-
-// eslint-disable-next-line no-undef
-
-
-//Blocos de código do projeto
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server online in port: ${PORT}`));
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -3,11 +3,11 @@ import joi from "joi";
 export const userSchema = joi.object({
     userName: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().required().min(),
+    password: joi.string().required(),
     address: joi.string().required()
 });
 
 export const loginSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required().min()
+    password: joi.string().required()
 });
