fd913dbca16d43deccd054f0ab8666206b9b0f04
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-05-01 15:26:36 -0500

    fix: user.rotes=>user.routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,6 @@
-import { Router } from "express";
-import authRouter from "./user.rotes.js";
-import shoppingRouter from "./shopping.routes.js";
+import { Router } from 'express';
+import authRouter from './user.routes.js';
+import shoppingRouter from './shopping.routes.js';
 
 const router = Router();
 router.use(authRouter);
