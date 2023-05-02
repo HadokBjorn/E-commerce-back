cfd3998aec5ffee8417e2007c76fe5cef93c4e69
Pedro <pedropiacentini@usp.br>
2023-05-02 13:03:49 -0300

    fix: exclusão de sessões expiradas do db

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -3,35 +3,37 @@ import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 
 export async function signUp(req, res) {
-	const { userName, email, password, address } = req.body;
+    const { userName, email, password, address } = req.body;
 
-	try {
-		const user = await db.collection('users').findOne({ email });
-		if (user) return res.status(409).send('E-mail já foi cadastrado!');
+    try {
+        const user = await db.collection('users').findOne({ email });
+        if (user) return res.status(409).send('E-mail já foi cadastrado!');
 
-		const hash = bcrypt.hashSync(password, 10);
+        const hash = bcrypt.hashSync(password, 10);
 
-		await db.collection('users').insertOne({ userName, email, address, password: hash });
-		res.sendStatus(201);
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+        await db.collection('users').insertOne({ userName, email, address, password: hash });
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
 
 export async function login(req, res) {
-	const { email, password } = req.body;
+    const { email, password } = req.body;
 
-	try {
-		const user = await db.collection('users').findOne({ email });
-		if (!user) return res.status(404).send('E-mail não cadastrado');
+    try {
+        const user = await db.collection('users').findOne({ email });
+        if (!user) return res.status(404).send('E-mail não cadastrado');
 
-		const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-		if (!isPasswordCorrect) return res.status(401).send('Senha incorreta');
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+        if (!isPasswordCorrect) return res.status(401).send('Senha incorreta');
 
-		const token = uuid();
-		await db.collection('sessions').insertOne({ token, userId: user._id });
-		res.send({ token, userName: user.userName });
-	} catch (err) {
-		res.status(500).send(err.message);
-	}
+        const token = uuid();
+
+        await db.collection('sessions').deleteOne({ userId: user._id });
+        await db.collection('sessions').insertOne({ token, userId: user._id });
+        res.send({ token, userName: user.userName });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
